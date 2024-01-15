9f6fc63dcaf5845d6ab942b1c344cb460db17643
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-01-14 20:37:30 -0400

    Seguridad formulario

--- src/main/java/com/tcna/primeraweb/config/WebMvcConfigurer.java
+++ src/main/java/com/tcna/primeraweb/config/WebMvcConfigurer.java
@@ -9,5 +9,6 @@ public class WebMvcConfigurer implements org.springframework.web.servlet.config.
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/403").setViewName("403");
+        registry.addViewController("/login").setViewName("login");
     }
 }
--- src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
+++ src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,16 +15,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails user1 = User.builder()
                 .username("user1")
-                .password("{bcrypt}$2a$10$55Ns1Itbqf/kffZY9Ru3o.Gudseuhuwz.fm4wWoT7T0GgoR5hoqqK")
+                .password("$2a$10$55Ns1Itbqf/kffZY9Ru3o.Gudseuhuwz.fm4wWoT7T0GgoR5hoqqK")
                 .roles("USER")
                 .build();
 
         UserDetails user2 = User.builder()
                 .username("admin")
-                .password("{bcrypt}$2a$10$55Ns1Itbqf/kffZY9Ru3o.Gudseuhuwz.fm4wWoT7T0GgoR5hoqqK")
+                .password("$2a$10$55Ns1Itbqf/kffZY9Ru3o.Gudseuhuwz.fm4wWoT7T0GgoR5hoqqK")
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user1,user2);
@@ -36,7 +43,8 @@ public class WebSecurityConfig {
                         .requestMatchers("/personas/nueva").hasAnyRole("ADMIN")
                         .requestMatchers("/personas/editar/*","/personas/eliminar/*").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form.loginPage("/login").permitAll())
+                .logout(l -> l.permitAll())
                 .exceptionHandling(e -> e.accessDeniedPage("/403"));
         return httpSecurity.build();
     }
